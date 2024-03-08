a73c1a6e8f3ac75b6afc15814fd264ef0c4efbe1
nesralla <nesralla@gmail.com>
2024-03-08 02:59:35 -0300

    fix

--- TodoApi/Models/Comment.cs
+++ TodoApi/Models/Comment.cs
@@ -9,7 +9,7 @@ namespace TodoApi.Models
     {
         public int Id { get; set; }
         public int TaskId { get; set; }
-        public string Author { get; set; }
+        public User Author { get; set; }
         public string Text { get; set; }
     }
 }
